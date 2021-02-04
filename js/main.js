2bdb3f2085ce69114d5a43453016f41b3d3a0bba
Jurate <jurate.rusteikyte87@gmail.com>
2021-02-04 23:06:41 +0100

    Clock desing and render clock #24 #27

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import {clock} from './components/clock.js'
+
 import {renderSocials} from './components/renderSocials.js';
 import {socialsData} from './data/socialsData.js';
 
@@ -5,7 +7,7 @@ import {renderProgressBars} from './components/renderProgressBars.js'
 import {progressBarData} from './data/progressBarData.js';
 
 
-
+clock('.clock', '12-14 18:00:00');
 renderProgressBars('.left-column', progressBarData);
 renderSocials('footer > .socials', socialsData);
 
